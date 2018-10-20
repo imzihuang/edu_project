779d316cdb50c5d6e46951468ab8c67d68a232e4
imzihuang <imaihuang@gmail.com>
2018-10-20 12:21:45 +0800

    update login

--- views/__init__.py
+++ views/__init__.py
@@ -9,7 +9,7 @@ def _handlers():
         prefix += '/'
     return [
         URLSpec('/', input.DefaultHandler, default_settings),
-        URLSpec('/', input.LoginViewHandler, default_settings),
+        URLSpec(prefix+'login.html', input.LoginViewHandler, default_settings),
         URLSpec(prefix+'(?P<manage_obj>.+).html', input.ManageViewHandler, default_settings),
         (prefix + r'(.*\.(css|png|gif|jpg|js|ttf|woff|woff2))', StaticFileHandler, {'path': default_settings.get('static_path')}),
         ]
