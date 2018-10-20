0e9f9bf4bb40eee455b036a8caf74dffb59f5b00
imzihuang <imaihuang@gmail.com>
2018-10-20 12:13:22 +0800

    add errorcode

---
+++ util/error_code.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# 正常返回
+ERROR_CODE_OK = 0
+# 没有检测到人脸
+ERROR_CODE_NO_FACE = 1
+# 人脸超出图片外
+ERROR_CODE_INTEGRAL_LOW = 2
+ERROR_CODE_NO_FACE = 1
+ERROR_CODE_NO_FACE = 1
+ERROR_CODE_NO_FACE = 1
