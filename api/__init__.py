785039f13c88c098e5e1cc5506a9d53d133fc18c
imzihuang <imaihuang@gmail.com>
2018-10-23 15:54:25 +0800

    update api

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ from api import input, update, infos, action, wx_action
 def _handlers():
     return [
         (r'/(?P<registry_obj>.+)/input$', input.RegistryHandler),
-        (r'/(?P<update_obj>.+)/infos$', update.UpdateHandler),
+        (r'/(?P<update_obj>.+)/updae$', update.UpdateHandler),
         (r'/(?P<infos_obj>.+)/infos$', infos.InfosHandler),
         (r'/(?P<action>.+)/action$', action.ActionHandler),
         (r'/(?P<action>.+)/wx_action$', wx_action.WXActionHandler),
