9dfe98961e4aeccf2ade3d92b309658e75e56e55
unknown <imzihuang@gmail.com>
2018-10-27 19:57:45 +0800

    update action

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,14 @@
 #coding:utf-8
 
 from api import input, update, infos, action, wx_action
+from settings import default_settings
 
 def _handlers():
     return [
         (r'/(?P<registry_obj>.+)/input$', input.RegistryHandler),
         (r'/(?P<update_obj>.+)/update$', update.UpdateHandler),
         (r'/(?P<infos_obj>.+)/infos$', infos.InfosHandler),
-        (r'/(?P<action>.+)/action$', action.ActionHandler),
+        (r'/(?P<action>.+)/action$', action.ActionHandler, default_settings),
         (r'/(?P<action>.+)/wx_action$', wx_action.WXActionHandler),
     ]
 
