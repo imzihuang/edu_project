d94ee0b8147621099b4f1035f2edaf4762d402e2
mzj <1260701194@qq.com>
2018-11-05 17:26:58 +0800

    modified:   util/face_util.py

--- util/face_util.py
+++ util/face_util.py
@@ -50,43 +50,43 @@ code,results = face_recognition_yyl.Face_Add(school_id,face_tokens)
 '''
 code,results = face_recognition_yyl.Face_Remove(school_id,face_tokens)
 
-
-def face_identy(file_path):
-    """
-    通过第三方api获取人脸特征
-    :param file_path: 图片路径
-    :return:
-    """
-    _file = open(file_path, 'rb').read()
-    files = {'image_file': _file}
-    # 人脸检测
-    data = {
-        'api_key': face_api_key,
-        'api_secret': face_api_secret,
-    }
-    response = requests.post(face_detect_url, data=data, files=files)
-    results = response.json()
-    if results.get('error_message'):
-        LOG.info("face++ identy error:%s"%results.get('error_message'))
-        return 1, results.get('error_message')
-
-    face_token = results['faces'][-1]['face_token']
-    return 0, face_token
-
-def create_face_tokenset(school_obj):
-    # 人脸库创建
-    data = {
-        'api_key': face_api_key,
-        'api_secret': face_api_secret,
-        'display_name': school_obj.get("name"),
-        'outer_id': school_obj.get("id"),
-        'tags': school_obj.get("id"),
-    }
-    response = requests.post(face_create_url, data)
-    results = response.json()
-    if results.get('error_message', ""):
-        LOG.error("create face tokenset error:%s" % results.get('error_message'))
-        return
-
-    faceset_token = results.get('faceset_token', "")  # faceset
-    return faceset_token
+#
+# def face_identy(file_path):
+#     """
+#     通过第三方api获取人脸特征
+#     :param file_path: 图片路径
+#     :return:
+#     """
+#     _file = open(file_path, 'rb').read()
+#     files = {'image_file': _file}
+#     # 人脸检测
+#     data = {
+#         'api_key': face_api_key,
+#         'api_secret': face_api_secret,
+#     }
+#     response = requests.post(face_detect_url, data=data, files=files)
+#     results = response.json()
+#     if results.get('error_message'):
+#         LOG.info("face++ identy error:%s"%results.get('error_message'))
+#         return 1, results.get('error_message')
+#
+#     face_token = results['faces'][-1]['face_token']
+#     return 0, face_token
+#
+# def create_face_tokenset(school_obj):
+#     # 人脸库创建
+#     data = {
+#         'api_key': face_api_key,
+#         'api_secret': face_api_secret,
+#         'display_name': school_obj.get("name"),
+#         'outer_id': school_obj.get("id"),
+#         'tags': school_obj.get("id"),
+#     }
+#     response = requests.post(face_create_url, data)
+#     results = response.json()
+#     if results.get('error_message', ""):
+#         LOG.error("create face tokenset error:%s" % results.get('error_message'))
+#         return
+#
+#     faceset_token = results.get('faceset_token', "")  # faceset
+#     return faceset_token
