1a1987f84ef6e2111bc2b5e2b046c9f748fbcaa7
unknown <liyanping@jw.hongshu.com>
2018-11-08 15:29:55 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,34 +1,20 @@
-$.extend({
-	majax:function(){
-	 var defaultOption = {
-	 	url:"",
-	 	jsonpCallback:'',
-	 	timeout:"30000",
-	 	contentType: "application/x-www-form-urlencoded;chartset=UTF-8",
-	 	data:{},
-	 	successCallBack:function() {},
-	 	errorCallBack:function() {},
-	 }
-	 $.ajax({
-	 	url:defaultOption.url,
-	 	data:defaultOption.data,
-	 	xhrFields: {
-	 		"withCredentials": true
-	 	},
-	 	type: 'POST',
-	 	jsonpCallback:defaultOption.jsonpCallback,
-	 	traditional: true,
-	 	contentType: defaultOption.contentType,
-	 	dataType: 'json',
-	 	cache: false,
-	 	timeout:defaultOption.timeout,
-	 	success:function(data){
-	 		defaultOption.successCallBack(data);
-	 	},
-	 	error:function(data){
-	 		defaultOption.errorCallBack(data);
-	 	}
-	 });
-    }
-});
-
+//获取学校id
+function schoolInput() {
+	var data = {
+		"limit": 20,
+		"offset": 1,
+	}
+	$.ajax({
+		type: "GET",
+		contentType: "application/x-www-form-urlencoded;chartset=UTF-8",
+		url: "/school/infos",
+		async: false,
+		data: data,
+		timeout: "30000",
+		success: function(msg) {
+			var msg = JSON.parse(msg);
+			console.log(msg);
+			school = msg.data[0].id;
+		}
+	})
+}
