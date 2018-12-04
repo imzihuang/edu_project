83642faf6d40e4e308d0a0aba977bfa42793271a
unknown <liyanping@jw.hongshu.com>
2018-12-04 18:53:40 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,24 +1,24 @@
 //获取学校id
-var school;
-var cardcode;
-
-function schoolInput() {
-	var data = {
-		"limit": 20,
-		"offset": 1,
-	}
-	$.ajax({
-		type: "GET",
-		contentType: "application/x-www-form-urlencoded;chartset=UTF-8",
-		url: "/school/infos",
-		async: false,
-		data: data,
-		timeout: "30000",
-		success: function(msg) {
-			var msg = JSON.parse(msg);
-			school = msg.data[0].id;
-			cardcode = msg.data[0].cardcode;
-		}
-	})
-}
+//var school;
+//var cardcode;
+//
+//function schoolInput() {
+//	var data = {
+//		"limit": 20,
+//		"offset": 1,
+//	}
+//	$.ajax({
+//		type: "GET",
+//		contentType: "application/x-www-form-urlencoded;chartset=UTF-8",
+//		url: "/school/infos",
+//		async: false,
+//		data: data,
+//		timeout: "30000",
+//		success: function(msg) {
+//			var msg = JSON.parse(msg);
+//			school = msg.data[0].id;
+//			cardcode = msg.data[0].cardcode;
+//		}
+//	})
+//}
 
