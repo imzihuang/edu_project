740aae8fdf50b7a381672496b4874ba7e25d7047
unknown <imzihuang@gmail.com>
2018-12-15 16:55:34 +0800

    update

--- views/input.py
+++ views/input.py
@@ -35,14 +35,21 @@ class LoginViewHandler(RequestHandler):
         self.render("login.html")
 
 class IndextHandler(RequestHandler):
-    def initialize(self, static_path, templates_path, **kwargs):
+    def initialize(self, static_path, templates_path, view_prefix, **kwargs):
         self.static_path = static_path
         self.templates_path = templates_path
+
+        if view_prefix[-1] != '/':
+            view_prefix += '/'
+            self.prefix = view_prefix
 
     def get_template_path(self):
         return self.templates_path
 
     def get(self):
+        if not self.get_secure_cookie('user_level', ""):
+            self.redirect(self.prefix + r'login.html', permanent=True)
+            return
         self.render("index.html")
 
 
