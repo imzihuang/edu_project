14f574410312eb359feca7d679df1fb68225d07f
imzihuang <imaihuang@gmail.com>
2019-01-10 10:51:07 +0800

    update excel

--- util/util_excel.py
+++ util/util_excel.py
@@ -5,26 +5,34 @@ import xlwt as ExcelWrite
 import xlrd as ExcelRead
 
 def make_student_header(sheet, student_relative_list):
-    row = 0
+    row_zero = 0
+    row_one = 1
     for student_dict in student_relative_list:
         col = 0
+        col_merge = 0
         for k in student_dict:
             if k!="relative_list":
                 if k == "name":
                     k="student_name"
                 if k not in excel_header:
                     continue
-                sheet.write(row, col, excel_header.get(k, k))
+                sheet.write(row_one, col, excel_header.get(k, k))
                 col += 1
+        sheet.write_merge(row_zero, row_zero, 0, col-1, "学生".decode('utf-8'))
+        col_merge = col - 1
         relative_list = student_dict.get("relative_list", [])
+        num = 1
         for relative_info in relative_list:
             for k in relative_info:
                 if k == "name":
                     k = "relative_name"
                 if k not in excel_header:
                     continue
-                sheet.write(row, col, excel_header.get(k, k))
+                sheet.write(row_one, col, excel_header.get(k, k))
                 col += 1
+            sheet.write_merge(row_zero, row_zero, col_merge, col - 1, "联系人".decode('utf-8')+str(num))
+            col_merge = col - 1
+            num += 1
 
 
 def make_student_excel(student_relative_list):
@@ -34,7 +42,7 @@ def make_student_excel(student_relative_list):
     sheet = xls.add_sheet("student_relative", cell_overwrite_ok=True)
 
     make_student_header(sheet, student_relative_list)
-    row = 1
+    row = 2
     #填充每行的数据
     for student_dict in student_relative_list:
         col = 0
